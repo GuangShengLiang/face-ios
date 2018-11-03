e42b553768ad1ddeabc58b35465a4175e408fe52
lgs <guangshengliang@gmail.com>
2018-11-03 10:41:20 +0800

    Initial Commit

---
+++ IOSFace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSFace
+//
+//  Created by lgs on 2018/11/3.
+//  Copyright © 2018年 lgs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
